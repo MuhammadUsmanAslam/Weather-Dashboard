8f63e5abca98e129806c8b176b3063aaf7e9c32d
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-03-16 00:51:20 +0500

    Add new-like and display likes-data in list items

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import './style.css';
-import { getData } from './modules/API.js';
+import { getData, getLikes, addLikes } from './modules/API.js';
 import { openPopup, closePopup } from './modules/comments.js';
 
-window.addEventListener('load', async () => {
+const mutateLikes = (weatherList, likesList) => {
+  weatherList.forEach((weatherListItem) => {
+    likesList.forEach((likesListItem) => {
+      if (weatherListItem.id === likesListItem.item_id) {
+        weatherListItem.likes = likesListItem.likes;
+      }
+    });
+  });
+  return weatherList;
+};
+
+const displayWeather = (dataList) => {
   const list = document.getElementById('list');
-  const dataList = await getData();
   dataList.forEach((element) => {
     const listItem = document.createElement('li');
     listItem.className = 'list-item';
-    listItem.innerHTML = `<img src='./assets/display-img.jpg'/>
-      <div class='list-item-head'><h4>${element.name}</h4><i class="fa-regular fa-heart"></i></div>
-      <p class='list-item-likes'>5 Likes</p>
-      `;
+    listItem.innerHTML = '<img src=\'./assets/display-img.jpg\'/>';
+    const listItemHead = document.createElement('div');
+    listItemHead.className = 'list-item-head';
+    listItemHead.innerHTML = `<h4>${element.name}</h4>`;
+    const likeBtn = document.createElement('i');
+    likeBtn.className = 'fa-regular fa-heart';
+    likeBtn.addEventListener('mouseover', () => {
+      likeBtn.className = 'fa-solid fa-heart';
+    });
+    likeBtn.addEventListener('mouseout', () => {
+      likeBtn.className = 'fa-regular fa-heart';
+    });
+    likeBtn.addEventListener('click', (e) => {
+      addLikes(element.id);
+      e.target.classList.remove('fa-solid');
+    });
+    listItemHead.appendChild(likeBtn);
+    listItem.appendChild(listItemHead);
+
+    const likes = document.createElement('p');
+    likes.className = 'list-item-likes';
+    likes.innerText = `${element.likes ? element.likes : 0} Likes`;
+    listItem.appendChild(likes);
+
     const button = document.createElement('button');
     button.innerText = 'Comments';
     button.className = 'comment-button';
@@ -21,6 +51,15 @@ window.addEventListener('load', async () => {
     listItem.appendChild(button);
     list.appendChild(listItem);
   });
+};
+
+window.addEventListener('load', async () => {
+  const likesData = await getLikes();
+  const dataList = await getData();
+
+  const newDataList = await mutateLikes(dataList, likesData);
+
+  displayWeather(newDataList);
 
   const closeBtns = document.querySelectorAll('.close-button');
   closeBtns.forEach((button) => {
--- src/modules/API.js
+++ src/modules/API.js
@@ -1,8 +1,32 @@
-/*eslint-disable*/
-export const getData = async () => {
+const getData = async () => {
   const API_KEY = '358eb180ada51f6235f6ecc20247a6d1';
   const URL = `https://api.openweathermap.org/data/2.5/find?lat=10&lon=75&APPID=${API_KEY}`;
   const res = await fetch(URL);
   const data = await res.json();
   return data.list;
 };
+
+const getLikes = async () => {
+  const APP_ID = 'zgPe1uPVdPIz90V51Wzt';
+  const URL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes`;
+  const res = await fetch(URL);
+  const likes = await res.json();
+  return likes;
+};
+
+const addLikes = async (id) => {
+  const APP_ID = 'zgPe1uPVdPIz90V51Wzt';
+  const URL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes`;
+  await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  });
+  getLikes();
+};
+
+module.exports = { getData, getLikes, addLikes };
