ce44b1ab812105b35804af622db343886a949a76
Esther Mumo <101511172+mumo-esther@users.noreply.github.com>
2023-03-15 11:42:43 +0300

    added a class to comment button

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ window.addEventListener('load', async () => {
     listItem.innerHTML = `<img src='./assets/display-img.jpg'/>
       <div class='list-item-head'><h4>${element.name}</h4><i class="fa-regular fa-heart"></i></div>
       <p class='list-item-likes'>5 Likes</p>
-      <button>Comments</button>`;
+      <button class="comment-button">Comments</button>`;
     list.appendChild(listItem);
   });
 });
